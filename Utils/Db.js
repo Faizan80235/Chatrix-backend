64805df755978eec19d776f73720e76c7c7143d7
Faizan80235 <fa3031980@gmail.com>
2025-09-05 19:56:53 +0500

    chnage my code

--- Utils/Db.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-
-// MongoDB Connect
-const ConnectDB= mongoose.connect('mongodb://127.0.0.1:27017/chat-app', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("✅ MongoDB Connected"))
-  .catch(err => console.error("❌ MongoDB Error:", err));
-module.exports=ConnectDB
